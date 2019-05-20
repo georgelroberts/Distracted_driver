14300cb43d74d6a753fefff54dc789c0a6b5e9af
George Roberts <georgeroberts@Georges-MacBook-Pro.local>
2019-05-20 17:50:14 +0100

    Skeleton main file and info files

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+Author: G. L. Roberts
+Date: May 2019
+
+About: Computer vision project to detect when a driver is distracted
+at the wheel.
+'''
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
